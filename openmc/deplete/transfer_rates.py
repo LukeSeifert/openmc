901594980916605029bd7b90e5c09d1587b71e1c
Luke Seifert <lukeseifert23@gmail.com>
2023-07-20 13:48:58 -0500

    index transfer indent shift

--- openmc/deplete/transfer_rates.py
+++ openmc/deplete/transfer_rates.py
@@ -239,20 +239,21 @@ class TransferRates:
                 self.transfer_rates[material_id][component] = dict()
             self.transfer_rates[material_id][component][destination_material_id] = \
                 transfer_rate / unit_conv
-            if destination_material_id is not None:
-                self.index_transfer.add((destination_material_id, material_id))
-                updated_index_transfer = self.index_transfer.copy()
-                # Check if material_id is already in index transfer
-                for transfer_tuple in self.index_transfer:
-                    # If it is, modify destinations
-                    if transfer_tuple[1] == material_id:
-                        updated_index_transfer.remove(transfer_tuple)
-                        # Check that destinations aren't repeated
-                        if destination_material_id not in transfer_tuple[0]:
-                            updated_destinations = transfer_tuple[0] + (destination_material_id,)
-                        else:
-                            updated_destinations = transfer_tuple[0]
-                        updated_index_transfer.add((updated_destinations, material_id))
+
+        if destination_material_id is not None:
+            self.index_transfer.add((destination_material_id, material_id))
+            updated_index_transfer = self.index_transfer.copy()
+            # Check if material_id is already in index transfer
+            for transfer_tuple in self.index_transfer:
+                # If it is, modify destinations
+                if transfer_tuple[1] == material_id:
+                    updated_index_transfer.remove(transfer_tuple)
+                    # Check that destinations aren't repeated
+                    if destination_material_id not in transfer_tuple[0]:
+                        updated_destinations = transfer_tuple[0] + (destination_material_id,)
                     else:
-                        updated_index_transfer.add((destination_material_id, material_id))
-                self.index_transfer = updated_index_transfer
+                        updated_destinations = transfer_tuple[0]
+                    updated_index_transfer.add((updated_destinations, material_id))
+                else:
+                    updated_index_transfer.add((destination_material_id, material_id))
+            self.index_transfer = updated_index_transfer
